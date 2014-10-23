beb3cb25d1e31ead32149d90f9dba39261dc2428
Alex Persian <alex_persian@me.com>
2014-10-23 13:53:34 -0500

    Reverted three.js test
    Reverted three.js test back to animated torus knot

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,6 @@
 // set the scene size
-var WIDTH = 800,
-    HEIGHT = 600;
+var WIDTH = 1000,
+    HEIGHT = 700;
 
 // set some camera attributes
 var VIEW_ANGLE = 45,
@@ -8,45 +8,54 @@ var VIEW_ANGLE = 45,
     NEAR = 0.1,
     FAR = 10000;
 
-// Renderer
-var renderer = new THREE.WebGLRenderer({
-    antialias: true
-});
-renderer.setSize(WIDTH, HEIGHT);
+// get the DOM element to attach to
+// - assume we have jQuery to hand
+var $container = $('#container');
 
-// Camera
+// create a WebGL renderer, camera
+// and a scene
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 var camera = new THREE.PerspectiveCamera(
     VIEW_ANGLE,
     ASPECT,
     NEAR,
     FAR);
-camera.position.z = 300;
 
-// Scene
 var scene = new THREE.Scene();
 
-// jQuery, append renderer to DOM
-var $container = $('#container');
+// add the camera to the scene
+scene.add(camera);
+
+// the camera starts at 0, 0, 0
+// set it back further
+camera.position.z = 300;
+
+// start the renderer
+renderer.setSize(WIDTH, HEIGHT);
+
+// attach the render-supplied DOM element
 $container.append(renderer.domElement);
 
-// Lighting
+var geometry = new THREE.TorusKnotGeometry( 70, 3, 100, 32 );
+var material = new THREE.MeshBasicMaterial( { color: 0xcc0000 } );
+var torusKnot = new THREE.Mesh( geometry, material );
+scene.add( torusKnot );
+
+// create a point light
 var pointLight = new THREE.PointLight(0xffffff);
 
+// set the light's position
 pointLight.position.x = 10;
 pointLight.position.y = 50;
 pointLight.position.z = 130;
 
-// Attempt at using boxes
-var geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
-var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-var torusKnot = new THREE.Mesh( geometry, material );
-
-scene.add( torusKnot );
-scene.add(camera);
+// add the light to the scene
 scene.add(pointLight);
 
-// Rendering
+
 function render() {
     requestAnimationFrame(render);
+    torusKnot.rotation.z += 0.01;
+    torusKnot.rotation.y += 0.1;
     renderer.render(scene, camera);
 }render();
