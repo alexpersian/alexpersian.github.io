130ed6ea553482278ee84e3e725c07e99a8e9d58
Alex Persian <alex_persian@me.com>
2014-10-29 15:37:07 -0500

    Terrain generator changes
    Added fog to the terrain and modified the generation algorithm.

--- javascripts/terrain.js
+++ javascripts/terrain.js
@@ -9,18 +9,18 @@ $(document).ready(function() {
     var meshWidth, meshHeight;
 
     function init() {
-        width = 1400;
+        width = window.innerWidth - 16;
         height = 700;
 
-        meshWidth = 20;
-        meshHeight = 20;
+        meshWidth = 10;
+        meshHeight = 10;
 
         // Scene
         scene = new THREE.Scene();
 
         // Camera
-        camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-        camera.position.z = 800;
+        camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100000);
+        camera.position.set(0, 0, 3000);
 
         // Controls
         controls = new THREE.OrbitControls(camera);
@@ -34,7 +34,7 @@ $(document).ready(function() {
         //light.position.set(-60, 20, 100);
 
         // Plane geometry
-        geometry = new THREE.PlaneGeometry(1200, 1200, meshWidth, meshHeight);
+        geometry = new THREE.PlaneGeometry(5000, 5000, meshWidth, meshHeight);
         texture = new THREE.ImageUtils.loadTexture("images/terrain-texture.png");
         material = new THREE.MeshBasicMaterial({
             //wireframe: true,
@@ -43,55 +43,60 @@ $(document).ready(function() {
         plane = new THREE.Mesh(geometry, material);
 
         scene.add(plane);
+        scene.fog = new THREE.FogExp2(0x9CDBFF, 0.0003);
 
         // Renderer
         renderer = new THREE.WebGLRenderer();
         renderer.setSize(width, height);
-        renderer.setClearColor(0xffffff);
+        renderer.setClearColor(0x9CDBFF);
         renderer.shadowMapEnabled = true;
 
         var $container = $('#container');
         $container.append(renderer.domElement);
 
-        draw_terrain();
-        render();
-        //animate();
-
-
     }init();
 
     function render() {
-        requestAnimationFrame(render);
+        draw_terrain();
         renderer.render(scene, camera);
     }
-
-    //function animate() {
-    //    requestAnimationFrame(animate);
-    //    renderer.render(scene, camera);
-    //    controls.update();
-    //}
 
     function draw_terrain() {
         var j = geometry.vertices.length;
         for (var i = 0; i < j; i++) {
-            geometry.vertices[i].z = (Math.pow((Math.random() + 0.1), -1)) * 10;
-
-          //  geometry.vertices[findIndex()] = (Math.seedrandom(i));
-
-
-
+            geometry.vertices[i].z = Math.floor(
+                3 * (Math.sin(Math.random())) *
+                3 * (Math.cos(Math.random())) * 50);
+            
+            //geometry.vertices[i].z = (Math.pow((Math.random() + 0.1), -1)) * 10;
+            //geometry.vertices[findIndex()] = (Math.seedrandom(i));
         }
     }
 
-    function randomize(rows, amplitude) {
-        for (var i = 0; i < rows; i++) {
-            Math.seedrandom(i);
-
-        }
-    }
+    //function randomize(rows, amplitude) {
+    //    for (var i = 0; i < rows; i++) {
+    //        Math.seedrandom(i);
+    //
+    //    }
+    //}
 
     function findIndex(x, y, m) {
         return x * m + y;
     }
+
+    window.requestAnimationFrame = (function() {
+        return window.requestAnimationFrame     ||
+            window.webkitRequestAnimationFrame  ||
+                window.mozRequestAnimationFrame ||
+                function(callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+
+    })();
+
+    (function animLoop() {
+        requestAnimationFrame(animLoop);
+        render();
+    })();
 
 });
