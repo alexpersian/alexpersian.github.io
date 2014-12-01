8abfe4391a4a6ff4b5bf94e4d48778007370ae4c
Alex Persian <alex_persian@me.com>
2014-12-01 15:48:42 -0600

    Tons of changes
    Added responsive navbar, fix attempt for Multiplayer FPS UI bug,
    translated paper.js functions from paperscript to Javascript

--- scripts/wb-paper.js
+++ scripts/wb-paper.js
@@ -2,24 +2,49 @@
  * Created by alexpersian on 11/16/14.
  */
 
-var myPath;
+// This file handles the drawing functions for whiteboard.js
 
-function onMouseDown(event) {
-    myPath = new Path();
-    myPath.add(event.point);
-    myPath.strokeColor = WBAPP.penColor;
-    myPath.strokeWidth = WBAPP.penStroke;
-    myPath.strokeCap = 'round';
-}
+paper.install(window);
+var PAPER = window.onload = function() {
+    paper.setup(WBAPP.canvas);
 
-function onMouseDrag(event) {
-    myPath.add(event.point);
-}
+    var localPaper = {};
 
-function onMouseUp(event) {
-    myPath.simplify();
-}
+    var myPath;
+    var tool = new Tool();
 
-var rectangle = new Rectangle(new Point(0, 0), new Point(WBAPP.width, WBAPP.height));
-var rectPath = new Path.Rectangle(rectangle);
-rectPath.fillColor = 'white';
+    tool.onMouseDown = function(event) {
+        myPath = new paper.Path({
+            strokeColor: WBAPP.penColor, // WBAPP is the global module from whiteboard.js
+            strokeWidth: WBAPP.penStroke,
+            strokeCap: 'round'
+        });
+        myPath.add(event.point);
+    };
+
+    tool.onMouseDrag = function(event) {
+        myPath.add(event.point);
+    };
+
+    tool.onMouseUp = function(event) {
+        if (!WBAPP.erasing) { myPath.simplify(); }
+    };
+
+    // Covers the canvas in a white rectangle to prevent transparent background on save.
+    // Drawings are created on top of this rectangle.
+    var rectangle = new paper.Rectangle(new paper.Point(0, 0), new paper.Point(WBAPP.width, WBAPP.height));
+    var rectPath = new paper.Path.Rectangle(rectangle);
+    rectPath.fillColor = 'white';
+
+    // TODO: Fix loading functionality
+    localPaper.loadRaster = function(image) {
+        console.log("fail)");
+        paper.remove();
+        var raster = new paper.Raster({
+            source: image,
+            position: view.center
+        });
+    };
+
+    return localPaper;
+};
--- scripts/whiteboard.js
+++ scripts/whiteboard.js
@@ -2,17 +2,14 @@
  * Created by alexpersian on 11/16/14.
  */
 
+// Main Javascript functions and code.
 var WBAPP = (function() {
+
+    paper.install(window);
 
     var wb = {};
 
     wb.canvas = document.getElementById("myCanvas");
-
-    //$("#myTab").find("li").click(function(){
-    //    var can = $(this).prop('id');
-    //    //wb.canvas = document.getElementById($(this).attribute('id'));
-    //    console.log(can);
-    //});
 
     wb.width = window.innerWidth;
     wb.height = window.innerHeight * 0.944;
@@ -26,19 +23,22 @@ var WBAPP = (function() {
     var prevColor, prevStroke;
 
     wb.changeColor = function (color) {
-        if (erasing === true) { wb.erase(); }
+        if (wb.erasing === true) { wb.erase(); }
         wb.penColor = color;
     };
 
-    var erasing = false;
-
+    // Hacky way to handle the eraser functionality.
+    // Draws over the canvas with a white pen stroke.
+    // Changes the text in the eraser button to reflect the mode.
+    // Saves the current pen color+width so it can be used after erasing is finished.
+    wb.erasing = false;
     wb.erase = function () {
-        if (erasing !== false) {
+        if (wb.erasing !== false) {
             wb.penColor = prevColor;
             wb.penStroke = prevStroke;
             document.getElementById("erase").innerHTML =
                 "<span class=\"glyphicon glyphicon-trash\" aria-hidden=\"true\"></span> Eraser OFF";
-            erasing = false;
+            wb.erasing = false;
         } else {
             prevColor = wb.penColor;
             prevStroke = wb.penStroke;
@@ -46,44 +46,67 @@ var WBAPP = (function() {
             wb.penStroke = wb.eraseStroke;
             document.getElementById("erase").innerHTML =
                 "<span class=\"glyphicon glyphicon-trash\" aria-hidden=\"true\"></span> Eraser ON";
-            erasing = true;
+            wb.erasing = true;
         }
     };
 
+    // Returns a date string used to timestamp save files.
     wb.getDate = function () {
-
         var d = new Date();
-
         var year = d.getFullYear();
         var month = d.getMonth() + 1;
         var day = d.getDate();
         var hour = d.getHours();
         var min = d.getMinutes();
         var sec = d.getSeconds();
-
         return (year + "-" + month + "-" + day + "_" + hour + ":" + min + ":" + sec);
     };
 
-    var button = document.getElementById('btn-save');
-    button.addEventListener('click', function(e) {
-        button.download = wb.getDate();
-        button.href = wb.canvas.toDataURL('image/png');
+    // Save button grabs canvas and saves it as .png with timestamp as file name.
+    var saveButton = document.getElementById('btn-save');
+    saveButton.addEventListener('click', function(e) {
+        saveButton.download = wb.getDate();
+        saveButton.href = wb.canvas.toDataURL('image/svg');
     });
 
-    $("#penWidth").change(function () {
-        var $dropdown = $(this);
-        wb.penStroke = $dropdown.val();
-    });
+    // Load a saved whiteboard file onto the canvas.
+    // TODO: Fix loading functionality
+    //var loadButton = document.getElementById('btn-load');
+    //loadButton.addEventListener('change', function(e) {
+    //
+    //    var reader = new FileReader();
+    //    reader.onload = function(event) {
+    //        console.log("omg freak out");
+    //        var img = new Image();
+    //        img.onload = function() {
+    //            PAPER.loadRaster(img);
+    //        };
+    //        img.src = event.target.result;
+    //    };
+    //    reader.readAsDataURL(this.files[0]);
+    //
+    //}, false);
 
-    $("#eraseWidth").change(function () {
-        var $dropdown = $(this);
-        wb.eraseStroke = $dropdown.val();
-        if (erasing === true) { wb.penStroke = wb.eraseStroke; }
-    });
-    
+    wb.changePenWidth = function(width) {
+        wb.penStroke = width;
+        document.getElementById('penDisplay').innerHTML = "Pen Width: " + width;
+    };
+
+    wb.changeEraserWidth = function(width) {
+        wb.eraseStroke = width;
+        if (wb.erasing == true) {wb.penStroke = wb.eraseStroke; }
+        document.getElementById('eraseDisplay').innerHTML = "Eraser Width: " + width;
+    };
+
+    // Ensures that drawings aren't unintentionally lost when navigating away from page.
     $(window).bind('beforeunload', function(){
         return "Save your drawing before reloading!";
     });
+
+    // Prevents default page scrolling action; fixes iOS 8 drawing bug.
+    document.addEventListener('touchmove', function(event) {
+        event.preventDefault();
+    }, false);
 
     return wb;
 }());
